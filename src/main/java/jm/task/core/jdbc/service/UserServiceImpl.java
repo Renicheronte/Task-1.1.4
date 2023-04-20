dca001dd2dd4ae39fd8bebeea9c5786026446aee
Rimasta <Renicheronte@yandex.ru>
2023-04-20 14:04:56 +0500

    Added annotations

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,26 +7,32 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
 
+    @Override
     public void createUsersTable() {
         userDaoJDBCImpl.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         userDaoJDBCImpl.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBCImpl.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         userDaoJDBCImpl.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return userDaoJDBCImpl.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDaoJDBCImpl.cleanUsersTable();
     }
